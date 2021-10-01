60b118d4281a39d608900eb690cf1d2078b0e836
zia_37058 <zhazy119@mymail.unisa.edu.au>
2021-10-01 21:18:38 +0930

    Using contract to review code

--- database.py
+++ database.py
@@ -46,23 +46,22 @@ class Data:
         except ValueError as v_error:
             Log(str(v_error), 'error', sys.exc_info()[2].tb_lineno)
 
-        Log('Query finished', 'info', sys._getframe().f_lineno)
+        Log('Query finished', 'info', sys._getframe().f_lineno) #
 
     @contracts.contract
     def data_query_batch(self, sql, value=None):
         """
         This is a function to upload many sql statements to database.
-        Sql only should be
-        'sql="'select {} from {}'",value=[(row_name,table_name)]'
-
         :param sql:
         :param value:
         :return:
         :type sql:str
         :type value: list[N],N=tuple
         """
+        #         Sql only should be
+        #         'sql="'select (?) from (?)'",value=[(row_name,table_name)]'
         try:
-            self.cursor.executemany(sql,)
+            self.cursor.executemany(sql,value)
 
 
         except sqlite3.Error:
@@ -77,16 +76,16 @@ class Data:
 
     @contracts.contract
     def add_data(self, table_name: str, data_list: tuple):
-        '''
+        """
 
         :param table_name:
         :param data_list:
         :return:
-        :type table_name:str
-        :type data_list:tuple(N),N=tuple
-        '''
+        :type table_name: str
+        :type data_list: tuple(N),N=tuple
+        """
 
-        __sql = 'insert into {} values {}'.format(table_name, data_list)
+        __sql = 'insert into "{}" values "{}"'.format(table_name, data_list)
         self.data_query(__sql)
 
     def update_data(self, table_name: str, data_list: tuple):
