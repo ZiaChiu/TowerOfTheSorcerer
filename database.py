5eaf76e098468aa1c49ea7f36c3bdbe602f19798
zia_37058 <zhazy119@mymail.unisa.edu.au>
2021-10-02 17:43:27 +0930

    add update function

--- database.py
+++ database.py
@@ -13,83 +13,186 @@ import contracts
 from log import Log
 
 
+class _DatabaseError(BaseException):
+    @contracts.contract
+    def __init__(self, info):
+        """
+        Any errors in the SQL statement.
+        :type info:str
+        :param info:
+        """
+        super().__init__(info)
+
+        self.info = info
+
+    def __str__(self):
+        return self.info
+
+
 class Data:
+    """
+    This class is used to operate the database.
+    """
+
     def __init__(self):
-        self.data = sqlite3.connect('example.db')
-        self.cursor = self.data.cursor()
+        self.__database = sqlite3.connect('data/data')
+        self.__cursor = self.__database.cursor()
 
     @contracts.contract
-    def data_query_alone(self, sql, value=None):
+    def data_query(self, sql, value=None):
         """
         This is a function to upload one sql statement to database.
-        Sql can has 2 format which are
-        'sql="select {} from {}".format(row_name,table_name)'
+        Sql should looks like
+        '
+        sql= f"select {row_name} from {table_name}"
+        self.cursor.execute(sql)
+        '
 
         :param sql:
         :param value:
         :return:
         :type sql:str
-        :type value: list[N],N=tuple
         """
         try:
             if value is None:
-                self.cursor.execute(sql)
-                self.data.commit()
+                self.__cursor.execute(sql)
+                self.__database.commit()
             else:
-                self.cursor.execute(sql, value)
-                self.data.commit()
+                if "(" in sql:
+                    if "?" in sql:
+                        if ")" in sql:
+
+                            self.__cursor.execute(sql, value)
+                            self.__database.commit()
+                        else:
+                            raise _DatabaseError('wrong SQL statement')
+                    else:
+                        raise _DatabaseError('wrong SQL statement')
+                else:
+                    raise _DatabaseError('wrong SQL statement')
         except sqlite3.Error:
             system = sys.exc_info()
             Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)
         except contracts.ContractException:
             Log('wrong value format or value!!!', 'error', sys.exc_info()[2].tb_lineno)
-        except ValueError as v_error:
-            Log(str(v_error), 'error', sys.exc_info()[2].tb_lineno)
+        except _DatabaseError as db_error:
+            Log(db_error.__str__(), 'error', sys.exc_info()[2].tb_lineno)
 
         Log('Query finished', 'info', sys._getframe().f_lineno)  # pylint: disable=W0212
 
     @contracts.contract
-    def data_query_batch(self, sql, value=None):
+    def add_value(self, table_name: str, value_list: list):
         """
-        This is a function to upload many sql statements to database.
-        :param sql:
-        :param value:
-        :return:
-        :type sql:str
-        :type value: list[N],N=tuple
-        """
-        #         Sql only should be
-        #         'sql="'select (?) from (?)'",value=[(row_name,table_name)]'
-        try:
-            self.cursor.executemany(sql, value)
-
-        except sqlite3.Error:
-            system = sys.exc_info()
-            Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)
-        except contracts.ContractException:
-            Log('wrong value format or value!!!', 'error', sys.exc_info()[2].tb_lineno)
-        except ValueError as v_error:
-            Log(str(v_error), 'error', sys.exc_info()[2].tb_lineno)
-
-        Log('Query finished', 'info', sys._getframe().f_lineno)  # pylint: disable=W0212
-
-    @contracts.contract
-    def add_data(self, table_name: str, data_list: tuple):
-        """
-
         :param table_name:
-        :param data_list:
+        :param value_list:
         :return:
         :type table_name: str
-        :type data_list: tuple(N),N=tuple
+        :type value_list: list[N],N=tuple
         """
+        try:
+            pass
+        except contracts.ContractException:
+            system = sys.exc_info()
+            Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
+        check_sql = f'select * from {table_name}'
+        data_value = self.__cursor.execute(check_sql)
+        data_format = data_value.fetchone()
+        row = '?'
 
-        __sql = 'insert into "{}" values "{}"'.format(table_name, data_list)
-        self.data_query_batch(__sql)
+        for index in range(len(data_format)):
+            if index == 0:
+                row += ',?,'
+            elif index == (len(data_format) - 1):
+                row += '?'
+            elif index < (len(data_format) - 2):
+                row += '?,'
 
-    def update_data(self, table_name: str, data_list: tuple):
-        __sql = 'update '
+        value_format = '(' + row + ')'
 
-    def closing_database(self):
-        self.cursor.close()
-        self.data.close()
+        try:
+            for value in value_list:
+                if len(value) != len(data_format):
+                    raise _DatabaseError('wrong tuple length!!!')
+
+            __sql = f'insert into {table_name} value {value_format}'
+            self.__cursor.executemany(__sql, value_list)
+            self.__database.commit()
+            Log('Query finished', 'info', sys._getframe().f_lineno)  # pylint: disable=W0212
+
+        except _DatabaseError as db_error:
+            Log(db_error.__str__(), category='error', line=sys.exc_info()[2].tb_lineno)  # pylint: disable=W0212
+
+        except sqlite3.Error:
+            system = sys.exc_info()
+            Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
+
+    @contracts.contract
+    def update_data(self, table_name: str, value_list: list):
+        """
+        This is the function used to execute SQLite's update statement.
+
+        :type table_name:str
+        :type value_list:list[]
+        """
+        try:
+            pass
+        except contracts.ContractSyntaxError:
+            system = sys.exc_info()
+            Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
+
+        try:
+            table = "'" + table_name + "'"
+            tables = self.__cursor. \
+                execute(f'SELECT count(*) FROM sqlite_master WHERE type="table" AND name = {table}').fetchone()
+            self.__database.commit()
+            if tables[0] == 0:
+                Log(str(tables), 'debug', sys._getframe().f_lineno)  # pylint: disable=W0212
+                raise _DatabaseError('table does not exist in db！！')
+        except sqlite3.Error:
+            system = sys.exc_info()
+            Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
+        except _DatabaseError as db_error:
+            Log(db_error.__str__(), 'error', sys.exc_info()[2].tb_lineno)  # pylint: disable=W0212
+
+        try:
+            row_names = f"PRAGMA table_info([{table_name}])"
+            row_check = self.__cursor.execute(row_names).fetchall()
+            self.__database.commit()
+            Log(str(row_check), 'info', sys._getframe().f_lineno)  # pylint: disable=W0212
+        except sqlite3.Error:
+            system = sys.exc_info()
+            Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
+
+        rows = self.__cursor.execute(row_names).fetchall()
+        row_list = []
+        for row in rows:
+            row_list.append(row[1])
+
+        Log(str(row_list), 'debug', sys._getframe().f_lineno)  # pylint: disable=W0212
+
+        try:
+            if len(value_list) != len(row_list):
+                raise _DatabaseError("The length of value_list does not match the length of db！！！")
+        except _DatabaseError:
+            system = sys.exc_info()
+            Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
+
+        value_dict = dict(zip(row_list, value_list))
+
+        Log(str(value_dict), 'debug', sys._getframe().f_lineno)  # pylint: disable=W0212
+        try:
+            for value in value_dict:
+                sql = f'update {table_name} SET {value}={value_dict[value]} where {row_list[0]}={value_list[0]}'
+                self.data_query(sql)
+        except sqlite3.Error:
+            system = sys.exc_info()
+            Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
+
+    def closing_db(self):
+        self.__cursor.close()
+        self.__database.close()
+
+
+db = Data()
+
+db.update_data(table_name="hero", id_row_name='name', value_list=["'Zia'", 0, 1, 1000, 10, 10, 10, 10, 10, 10])
