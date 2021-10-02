42327a07d12fd4f271f600efbf389244a83934fc
zia_37058 <zhazy119@mymail.unisa.edu.au>
2021-10-02 18:52:09 +0930

    add some comments and Docstrings

--- database.py
+++ database.py
@@ -40,6 +40,7 @@ class Data:
 
     @contracts.contract
     def data_query(self, sql, value=None):
+        # example: self.data_query(sql="select * from hero")
         """
         This is a function to upload one sql statement to database.
         Sql should looks like
@@ -128,6 +129,8 @@ class Data:
 
     @contracts.contract
     def update_all(self, table_name: str, value_list: list):
+        # example:
+        # db.update_all(table_name="hero", value_list=["'Zia'", 0, 1, 1000, 10, 10, 10, 10, 10, 10])
         """
         This is the function used to execute SQLite's update statement.
 
@@ -140,10 +143,12 @@ class Data:
             system = sys.exc_info()
             Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
 
+        # Check if the table name exists.
         try:
             table = "'" + table_name + "'"
             tables = self.__cursor. \
-                execute(f'SELECT count(*) FROM sqlite_master WHERE type="table" AND name = {table}').fetchone()
+                execute(f'SELECT count(*) FROM sqlite_master '
+                        f'WHERE type="table" AND name = {table}').fetchone()
             self.__database.commit()
             if tables[0] == 0:
                 Log(str(tables), 'debug', sys._getframe().f_lineno)  # pylint: disable=W0212
@@ -154,20 +159,13 @@ class Data:
         except _DatabaseError as db_error:
             Log(db_error.__str__(), 'error', sys.exc_info()[2].tb_lineno)  # pylint: disable=W0212
 
-        try:
-            row_names = f"PRAGMA table_info([{table_name}])"
-            row_check = self.__cursor.execute(row_names).fetchall()
-            self.__database.commit()
-            Log(str(row_check), 'info', sys._getframe().f_lineno)  # pylint: disable=W0212
-        except sqlite3.Error:
-            system = sys.exc_info()
-            Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
-
-        rows = self.__cursor.execute(row_names).fetchall()
+        rows = self.__cursor.execute(f"PRAGMA table_info([{table_name}])").fetchall()
         row_list = []
         for row in rows:
             row_list.append(row[1])
 
+        # Check whether the length of the input list
+        # is the same as the length of the list of the column name of the db.
         try:
             if len(value_list) != len(row_list):
                 raise _DatabaseError("The length of value_list does not match the length of db！！！")
@@ -176,40 +174,54 @@ class Data:
             Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
 
         value_dict = dict(zip(row_list, value_list))
-
-        Log(str(value_dict), 'debug', sys._getframe().f_lineno)  # pylint: disable=W0212
+        # update db
         try:
             for value in value_dict:
-                sql = f'update {table_name} SET {value}={value_dict[value]} where {row_list[0]}={value_list[0]}'
+                sql = f'update {table_name} ' \
+                      f'SET {value}={value_dict[value]} ' \
+                      f'where {row_list[0]}={value_list[0]}'
                 self.data_query(sql)
         except sqlite3.Error:
             system = sys.exc_info()
             Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
 
     @contracts.contract
-    def update_row(self, table_name: str,ID,row_list:list, value_list: list):
+    def update_row(self, table_name: str,
+                   primary_id,
+                   row_list: list,
+                   value_list: list):
+        # example:
+        # db.update_row(table_name='hero',
+        #               ID="'Zia'",
+        #               row_list=['exp','gold'],
+        #               value_list=[100,100])
         """
         This is the function used to execute SQLite's update statement.
 
-        :param ID: the primary_key 's value of db's table.
+        :param primary_id: the primary_key 's value of db's table.
         :type row_list:list[]
         :type table_name:str
         :type value_list:list[id]
         # value_list should looks like [,column1,column2...columnN]
         """
+
+        # Intercepted the Contract Syntax Error from contracts package
         try:
             pass
         except contracts.ContractSyntaxError:
             system = sys.exc_info()
             Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
 
+        # Intercepted db Errors from SQLite3 database
+        # Intercepted the _DatabaseError error
+        # Check if the table name exists.
         try:
             table = "'" + table_name + "'"
             tables = self.__cursor. \
-                execute(f'SELECT count(*) FROM sqlite_master WHERE type="table" AND name = {table}').fetchone()
+                execute(f'SELECT count(*) FROM sqlite_master '
+                        f'WHERE type="table" AND name = {table}').fetchone()
             self.__database.commit()
             if tables[0] == 0:
-                Log(str(tables), 'debug', sys._getframe().f_lineno)  # pylint: disable=W0212
                 raise _DatabaseError('table does not exist in db！！')
         except sqlite3.Error:
             system = sys.exc_info()
@@ -217,19 +229,7 @@ class Data:
         except _DatabaseError as db_error:
             Log(db_error.__str__(), 'error', sys.exc_info()[2].tb_lineno)  # pylint: disable=W0212
 
-        try:
-            row_names = f"PRAGMA table_info([{table_name}])"
-            row_check = self.__cursor.execute(row_names).fetchall()
-            self.__database.commit()
-            Log(str(row_check), 'info', sys._getframe().f_lineno)  # pylint: disable=W0212
-        except sqlite3.Error:
-            system = sys.exc_info()
-            Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
-
         rows = self.__cursor.execute(f"PRAGMA table_info([{table_name}])").fetchone()
-        # row_list = []
-        # for row in rows:
-        #     row_list.append(row[1])
 
         try:
             if len(value_list) != len(row_list):
@@ -240,21 +240,19 @@ class Data:
 
         value_dict = dict(zip(row_list, value_list))
 
-        Log(str(value_dict), 'debug', sys._getframe().f_lineno)  # pylint: disable=W0212
+        # Intercepted db Errors from SQLite3 database
+        # Intercepted the _DatabaseError error
+        # update db
         try:
             for value in value_dict:
-                sql = f'update {table_name} SET {value}={value_dict[value]} where {rows[1]}={ID}'
+                sql = f'update {table_name} SET {value}={value_dict[value]} ' \
+                      f'where {rows[1]}={primary_id}'
                 self.data_query(sql)
         except sqlite3.Error:
             system = sys.exc_info()
             Log(str(system[0]) + str(system[1]), category='error', line=system[2].tb_lineno)  # pylint: disable=W0212
 
     def closing_db(self):
+        """ Close the connection with the database"""
         self.__cursor.close()
         self.__database.close()
-
-
-db = Data()
-
-# db.update_all(table_name="hero", value_list=["'Zia'", 0, 1, 1000, 10, 10, 10, 10, 10, 10])
-# db.update_row(table_name='hero', ID="'Zia'",row_list=['exp','gold'],value_list=[100,100])
