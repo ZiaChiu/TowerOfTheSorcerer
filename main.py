9d1043941db705a7d7bb57ccea9763f3465f703d
zia_37058 <zhazy119@mymail.unisa.edu.au>
2021-10-14 16:22:16 +1030

    change pyqt5 to tk

--- main.py
+++ main.py
@@ -5,3 +5,21 @@ Website： www.crazyziye.top
 license：GPL
 Created DATE: 29/9/21 10:34 pm
 """
+
+from game import *
+
+
+class MainGame(Game):
+
+    def __init__(self):
+        super().__init__()
+
+
+def main():
+    ImageLibrary.load("data/image")
+    mainWindows = MainGame()
+    mainWindows.run()
+
+
+if __name__ == '__main__':
+    main()
